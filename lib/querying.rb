9a3b33c3c4398dc1ed8be44b5ebac4cf799378b0
Tandi Samples <tandisamples@gmail.com>
2019-11-12 01:53:49 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -10,7 +10,8 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  ""
+  "SELECT species, COUNT(*) FROM characters GROUP BY species
+   ORDER BY COUNT(species) DESC LIMIT 1"
 end
 
 def select_name_and_series_subgenres_of_authors
