ffb28d5d7744f25121a85c52eec4e2e0ca71dc6c
Tandi Samples <tandisamples@gmail.com>
2019-11-12 01:53:34 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -4,12 +4,13 @@ def select_books_titles_and_years_in_first_series_order_by_year
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  ""
+  "SELECT name, motto FROM characters ORDER BY LENGTH(motto)
+   DESC LIMIT 1"
 end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  ""
 end
 
 def select_name_and_series_subgenres_of_authors
